593699aa204be4831d76ca45c74e052f779e5142
parkash067 <pk_bscs@yahoo.com>
2018-12-24 16:19:38 +0500

    add template module for quotation

---
+++ ga_quotation_template_selector/__manifest__.py
@@ -0,0 +1,11 @@
+{
+	'name' : 'Auto Selector of Quotation Templates',
+	'version' : '1.1',
+	'depends' : ['base', 'sale', 'mail'],
+	'data' : ['views/mail_template_view.xml'],
+	'author' : 'Gerrys Apps',
+	'category' : '',
+	'website' : "www.gerrys.net",
+	'installable' : True,
+	'auto_install' : False,
+}	
---
+++ ga_quotation_template_selector/models/sale.py
@@ -0,0 +1,20 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+    
+    @api.multi
+    def action_quotation_send(self):
+        result = super(SaleOrder, self).action_quotation_send()
+        template_id = self.env['mail.template'].search([('company_id_ga', '=', self.env.uid.company_id),('model_id.model', '=', 'sale.order'),
+                                                        ('name', 'ilike', 'Send quotation')])
+        result['context']['template_id'] = template_id.id
+        return result
+
+
+class EmailTemplate(models.Model):
+    _inherit = "mail.template"
+    
+    company_id_ga = fields.Many2one('res.company', 'Company')
