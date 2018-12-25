faab174810e83eab64ce51fd7eedb6a04eb1cb1e
Kashif Aziz <kashifaziz123@hotmail.com>
2018-12-25 15:23:37 +0500

    update approval

--- ga_sale_order_approval/models/models.py
+++ ga_sale_order_approval/models/models.py
@@ -106,7 +106,7 @@ class InheritSaleOrder(models.Model):
 
     @api.multi
     def action_send_for_approval(self):
-        self.write({'approval_status': 'Waiting For Approval'})
+        self.sudo().write({'approval_status': 'Waiting For Approval'})
 
 
     @api.one
