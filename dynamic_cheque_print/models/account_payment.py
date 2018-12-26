12b8439a4a8f0311293969e5247dae5739dbd1dd
parkash067 <pk_bscs@yahoo.com>
2018-12-26 17:57:56 +0500

    remove dc

--- dynamic_cheque_print/models/account_payment.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, fields, models, _
-
-
-class field_account_payment(models.Model):
-    _inherit = 'account.payment'
-    
-    check_cash = fields.Boolean(string="Cash Cheque")
-    company_bank_cheque = fields.Boolean(string="Bank/Company Cheque")
-    to_journal = fields.Boolean(string='To Journal', default=True)
