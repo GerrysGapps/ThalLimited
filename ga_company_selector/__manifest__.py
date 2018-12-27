98a61062725e73a17fc9ce702b7571f2744a4130
parkash067 <pk_bscs@yahoo.com>
2018-12-27 17:08:07 +0500

    add option for company selection on incoming email server

---
+++ ga_company_selector/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Company Selector On Incoming Mail Server',
+    'version': '1.0',
+    'depends': ['base', 'mail'],
+    'data': ['views/incoming_server_view.xml'],
+    'author': 'Gerrys Apps',
+    'category': '',
+    'website': "www.gerrys.net",
+    'installable': True,
+    'auto_install': False,
+}
