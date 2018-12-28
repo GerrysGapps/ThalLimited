3c9ee2f5457263eb9a72d127cd8b21aa09810730
parkash067 <pk_bscs@yahoo.com>
2018-12-28 10:49:33 +0500

    remove payment enhancement module after successfully installed on master branch

--- ga_payments_enhancement/models/account_move.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-
-class AccountMove(models.Model):
-    _inherit = "account.move"
-
-    sale_order_id = fields.Many2one('sale.order', readonly=True)
-    purchase_order_id = fields.Many2one('purchase.order', readonly=True)
--- ga_payments_enhancement/models/account_tax.py
+++
@@ -1,8 +0,0 @@
-from odoo import api, fields, models, _
-
-
-class accountTax(models.Model):
-    _inherit = 'account.tax'
-
-    sales_income_wht = fields.Selection([('sales_WHT', 'Sales WHT'), ('income_WHT', 'Income WHT')],
-                                        string='Withholding Tax Type')
