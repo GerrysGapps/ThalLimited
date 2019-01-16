c2ff77c83fd61a69630bea8b76f15f214d3e3425
Kashif Aziz <kashifaziz123@hotmail.com>
2019-01-16 17:39:49 +0500

    ssdsd

--- ga_thal_customization/models/models.py
+++ ga_thal_customization/models/models.py
@@ -10,6 +10,169 @@ import datetime
 from odoo.tools.misc import clean_context
 
 
+class TopmanagementReport(models.TransientModel):
+    _name = 'topmanagement.report'
+
+    datetime = fields.Datetime()
+
+    @api.model
+    def send_report_to_topmanagement(self):
+        self.env['topmanagement.report'].sudo().create({'datetime': datetime.datetime.now()})
+        # template = self.env['mail.template'].search([('name','=','Send report')])
+        # template.send_mail(self.id, force_send=True)
+
+    @api.model
+    def get_details(self):
+        report_data_list = []
+        companies = self.env['res.company'].search([('id', '>', 0)])
+        for company in companies:
+            report_data_dict = {}
+            report_data_dict['company'] = company.name
+            self.env.cr.execute("select count(id) from crm_lead  where company_id=" + str(
+                company.id) + " and type='lead' and create_date>=" + "'" + fields.Datetime.to_string(
+                datetime.date.today() - datetime.timedelta(
+                    6)) + "'" + " and create_date<=" + "'" + fields.Datetime.to_string(datetime.datetime.now()) + "'")
+            total_lead_count = self.env.cr.dictfetchall()
+            self.env.cr.execute("select count(id) from crm_lead  where company_id=" + str(
+                company.id) + " and type ='opportunity' and date_conversion>=" + "'" + fields.Datetime.to_string(
+                datetime.date.today() - datetime.timedelta(
+                    6)) + "'" + " and date_conversion<=" + "'" + fields.Datetime.to_string(
+                datetime.datetime.now()) + "'")
+            converted_opportunity_count = self.env.cr.dictfetchall()
+            self.env.cr.execute("""select count(id) from crm_lead  where company_id='%s' and type='%s' and won_status='%s' and date_last_stage_update between '%s' and '%s'""" % (
+                    company.id, 'opportunity', 'pending',
+                    fields.Datetime.to_string(datetime.datetime.now() - datetime.timedelta(6)),
+                    fields.Datetime.to_string(datetime.datetime.now())))
+            open_opportunity_count = self.env.cr.dictfetchall()
+            self.env.cr.execute(
+                """select sum(actual_revenue) as initial,sum(planned_revenue) as revise from crm_lead  where company_id='%s' and type='%s' and won_status='%s' and date_last_stage_update between '%s' and '%s'""" % (
+                    company.id, 'opportunity', 'won',
+                    fields.Datetime.to_string(datetime.datetime.now() - datetime.timedelta(6)),
+                    fields.Datetime.to_string(datetime.datetime.now())))
+            won_opportunity_params = self.env.cr.dictfetchall()
+            self.env.cr.execute(
+                "select count(id) from crm_lead  where company_id=" + str(
+                    company.id) + " and type ='opportunity' and won_status = 'won' and date_last_stage_update>=" + "'" + fields.Datetime.to_string(
+                    datetime.datetime.now() - datetime.timedelta(
+                        6)) + "'" + " and date_last_stage_update<=" + "'" + fields.Datetime.to_string(
+                    datetime.datetime.now()) + "'")
+            won_opportunity_count = self.env.cr.dictfetchall()
+            self.env.cr.execute(
+                "select count(id) from crm_lead  where company_id=" + str(
+                    company.id) + " and type ='opportunity' and won_status = 'lost' and date_last_stage_update>=" + "'" + fields.Datetime.to_string(
+                    datetime.datetime.now() - datetime.timedelta(
+                        6)) + "'" + " and date_last_stage_update<=" + "'" + fields.Datetime.to_string(
+                    datetime.datetime.now()) + "'")
+            lost_opportunity_count = self.env.cr.dictfetchall()
+            if won_opportunity_params[0]['revise']:
+                report_data_dict['revise_revenue'] = won_opportunity_params[0]['revise']
+            else:
+                report_data_dict['revise_revenue'] = 0
+            if won_opportunity_params[0]['initial']:
+                report_data_dict['initial_revenue'] = won_opportunity_params[0]['initial']
+            else:
+                report_data_dict['initial_revenue'] = 0
+            if len(won_opportunity_count) > 0:
+                report_data_dict['won_opportunity_count'] = won_opportunity_count[0]['count']
+            else:
+                report_data_dict['won_opportunity_count'] = 0
+
+            if len(lost_opportunity_count) > 0:
+                report_data_dict['lost_opportunity_count'] = lost_opportunity_count[0]['count']
+            else:
+                report_data_dict['lost_opportunity_count'] = 0
+
+            if open_opportunity_count[0]['count'] > 0:
+                report_data_dict['open_opportunity_count'] = open_opportunity_count[0]['count']
+            else:
+                report_data_dict['open_opportunity_count'] = 0
+            if total_lead_count[0]['count'] > 0:
+                if converted_opportunity_count[0]['count'] > 0:
+                    report_data_dict['total_lead_count'] = total_lead_count[0]['count'] + \
+                                                           converted_opportunity_count[0]['count']
+                else:
+                    report_data_dict['total_lead_count'] = total_lead_count[0]['count']
+            else:
+                report_data_dict['total_lead_count'] = 0
+            if converted_opportunity_count[0]['count'] > 0:
+                report_data_dict['converted_opportunity_count'] = converted_opportunity_count[0]['count']
+            else:
+                report_data_dict['converted_opportunity_count'] = 0
+
+            if total_lead_count[0]['count'] > 0 and converted_opportunity_count[0]['count'] > 0:
+                conversion_ratio = converted_opportunity_count[0]['count'] / (
+                        total_lead_count[0]['count'] + converted_opportunity_count[0]['count']) * 100
+                report_data_dict['conversion_ratio'] = round(conversion_ratio, 2)
+            else:
+                report_data_dict['conversion_ratio'] = 0
+
+            self.env.cr.execute(
+                """select count(crm.id),sum(planned_revenue) as Current,sum(actual_revenue) as Initial, p.name from crm_lead crm INNER JOIN res_users res 
+                 ON crm.user_id= res.id  INNER JOIN res_partner p on res.partner_id = p.id 
+               where crm.company_id='%s' and crm.type='%s' and crm.won_status='%s' and crm.date_last_stage_update between '%s' and '%s'""" % (
+                    company.id, 'opportunity', 'pending',
+                    fields.Datetime.to_string(datetime.datetime.now() - datetime.timedelta(6)),
+                    fields.Datetime.to_string(datetime.datetime.now()))+" group by p.name ")
+            get_all_sales_data = self.env.cr.dictfetchall()
+
+            self.env.cr.execute(
+                """select count(crm.id),sum(planned_revenue) as Current,sum(actual_revenue) as Initial, p.name from crm_lead crm INNER JOIN res_users res 
+                 ON crm.user_id= res.id  INNER JOIN res_partner p on res.partner_id = p.id 
+               where crm.company_id='%s' and crm.type='%s' and crm.won_status='%s' and crm.date_last_stage_update between '%s' and '%s'""" % (
+                    company.id, 'opportunity', 'won',
+                    fields.Datetime.to_string(datetime.datetime.now() - datetime.timedelta(6)),
+                    fields.Datetime.to_string(datetime.datetime.now())) + " group by p.name ")
+            get_all_sales_data_won = self.env.cr.dictfetchall()
+
+            self.env.cr.execute(
+                """select count(lost_reason.id),lost_reason.name as lost_reason,sum(planned_revenue) as Current,sum(actual_revenue) as Initial from crm_lead crm INNER JOIN crm_lost_reason lost_reason ON crm.lost_reason=lost_reason.id where
+                crm.company_id='%s' and crm.type='%s' and crm.won_status='%s' and crm.date_last_stage_update between '%s' and '%s'""" % (
+                    company.id, 'opportunity', 'lost',
+                    fields.Datetime.to_string(datetime.datetime.now() - datetime.timedelta(6)),
+                    fields.Datetime.to_string(datetime.datetime.now())) + " group by lost_reason.name ")
+            get_all_sales_lost = self.env.cr.dictfetchall()
+            list_sales_person_lost = []
+            list_sales_person = []
+            list_sales_person_won = []
+            if len(get_all_sales_lost) > 0:
+                for record_sale_person_lost in get_all_sales_lost:
+                    print(record_sale_person_lost)
+                    sale_person_wise_lost = {}
+                    sale_person_wise_lost['current_expected_revenue'] = record_sale_person_lost['current']
+                    sale_person_wise_lost['initial_expected_revenue'] =record_sale_person_lost['initial']
+                    sale_person_wise_lost['difference_amount'] =record_sale_person_lost['initial'] - \
+                                                               record_sale_person_lost['current']
+                    sale_person_wise_lost['count_opportunity'] =record_sale_person_lost['count']
+                    sale_person_wise_lost['lost_reason'] = record_sale_person_lost['lost_reason']
+                    list_sales_person_lost.append(sale_person_wise_lost)
+
+            if len(get_all_sales_data)>0:
+                for record_sale_person_won in get_all_sales_data_won:
+                    sale_person_wise_won = {}
+                    sale_person_wise_won['current_expected_revenue'] = record_sale_person_won['current']
+                    sale_person_wise_won['initial_expected_revenue'] =record_sale_person_won['initial']
+                    sale_person_wise_won['difference_amount'] = record_sale_person_won['initial'] -record_sale_person_won['current']
+                    sale_person_wise_won['count_opportunity'] = record_sale_person_won['count']
+                    sale_person_wise_won['sale_person_name'] = record_sale_person_won['name']
+                    list_sales_person_won.append(sale_person_wise_won)
+
+            if len(get_all_sales_data)>0:
+                for record_sale_person in get_all_sales_data:
+                    sale_person_wise = {}
+                    sale_person_wise['current_expected_revenue'] = record_sale_person['current']
+                    sale_person_wise['initial_expected_revenue'] = record_sale_person['initial']
+                    sale_person_wise['difference_amount'] = record_sale_person['initial'] -record_sale_person['current']
+                    sale_person_wise['count_opportunity'] = record_sale_person['count']
+                    sale_person_wise['sale_person_name'] = record_sale_person['name']
+                    list_sales_person.append(sale_person_wise)
+            report_data_dict['sale_person_wise_open_lead'] = list_sales_person
+            report_data_dict['sale_person_wise_won_lead'] = list_sales_person_won
+            report_data_dict['lost_reason_wise_lead'] = list_sales_person_lost
+            report_data_list.append(report_data_dict)
+        if len(report_data_list) > 0:
+            return report_data_list
+
+
 class InheritCustomer(models.Model):
     _inherit = 'res.partner'
 
@@ -17,8 +180,9 @@ class InheritCustomer(models.Model):
     customer_category = fields.Selection(
         [('Dealer', 'Dealer'), ('Non Dealer', 'Non Dealer'), ('Corporate', 'Corporate'), ('Region', 'Region'),
          ('Associate Comapnies',
-          'Associate Comapnies'), ('Direct Customer', 'Direct Customer'),('Corporate Client', 'Corporate Client'), ('Export', 'Export')], string='Customer Category')
-    sales_office =fields.Selection(
+          'Associate Comapnies'), ('Direct Customer', 'Direct Customer'), ('Corporate Client', 'Corporate Client'),
+         ('Export', 'Export')], string='Customer Category')
+    sales_office = fields.Selection(
         [('3000', '3000'), ('4000', '4000')],
         string='Sales Office')
     distribution_channel = fields.Selection(
@@ -56,10 +220,12 @@ class DistributionChannel(models.Model):
 class CustomBank(models.Model):
     _name = 'custom.bank'
 
+
 class CustomPort(models.Model):
     _name = 'custom.ports'
 
     name = fields.Char('Description')
+
 
 class SalesOffice(models.Model):
     _name = 'sale.office'
@@ -73,6 +239,7 @@ class InheritProducttemplate(models.Model):
     old_material_no = fields.Char('Old Material No')
     default_code = fields.Char('Product Code', index=True)
 
+
 class inheritSaleOrderline(models.Model):
     _inherit = 'sale.order.line'
 
@@ -82,6 +249,7 @@ class inheritSaleOrderline(models.Model):
             self.price_unit = 0.0
             return
 
+
 class InheritSaleOrder(models.Model):
     _inherit = "sale.order"
 
@@ -89,12 +257,12 @@ class InheritSaleOrder(models.Model):
     #                              states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}, copy=False,
     #                              default=fields.Datetime.now.date())
 
-    shipment_mode =fields.Selection(
-        [('BY SEA VESSEL', 'BY SEA VESSEL'), ('BY AIR', 'BY AIR'),('BY ROAD', 'BY ROAD')],
+    shipment_mode = fields.Selection(
+        [('BY SEA VESSEL', 'BY SEA VESSEL'), ('BY AIR', 'BY AIR'), ('BY ROAD', 'BY ROAD')],
         string='Shipment Mode')
     delivery_period = fields.Char('Delivery Period')
-    port_of_shipment =fields.Char(string='POF Shipment',old='port_of_shipment')
-    port_of_dest = fields.Char(string='POF Destination',old='port_of_dest')
+    port_of_shipment = fields.Char(string='POF Shipment', old='port_of_shipment')
+    port_of_dest = fields.Char(string='POF Destination', old='port_of_dest')
     conversion_rate = fields.Float('Conversion Rate')
     type = fields.Char('Type')
     credit_limit = fields.Boolean('Within Credit Limit ?')
@@ -110,7 +278,6 @@ class InheritSaleOrder(models.Model):
             'view_mode': 'form',
             'target': 'new',
         }
-
 
     def send_requesttosap_func(self):
         self._check_customer_validations()
@@ -129,7 +296,7 @@ class InheritSaleOrder(models.Model):
     def _check_product_code_validations(self):
         for order in self.order_line:
             if not order.product_id.product_tmpl_id.default_code:
-                raise Warning(_('Please Provide Product Code For '+order.product_id.name))
+                raise Warning(_('Please Provide Product Code For ' + order.product_id.name))
 
     @api.one
     def _check_customer_validations(self):
@@ -197,12 +364,13 @@ class InheritSaleOrder(models.Model):
             '\t\t LAMINATES DIVIION'
 
         if vals['pricelist_id']:
-            pricelist = self.env['product.pricelist'].search([('id','=',vals['pricelist_id'])])
+            pricelist = self.env['product.pricelist'].search([('id', '=', vals['pricelist_id'])])
             if pricelist:
                 if 'PKR' not in pricelist.currency_id.name and 'BLD - Formite' in self.env.user.company_id.name:
                     vals['note'] = terms
                 else:
-                    res_config = self.env['res.config.settings'].search([('company_id','=',self.env.user.company_id.id)],order='id DESC',limit=1)
+                    res_config = self.env['res.config.settings'].search(
+                        [('company_id', '=', self.env.user.company_id.id)], order='id DESC', limit=1)
                     if res_config:
                         vals['note'] = res_config.sale_note
 
@@ -215,8 +383,10 @@ class InheritSaleOrder(models.Model):
                                                                           datetime.datetime.now().date())])
             if vals['conversion_rate'] and 'PKR' not in price_list_object.currency_id:
                 if not currency_record:
-                    self.env['res.currency.rate'].sudo().create({'name': datetime.datetime.now().date(),'company_id':self.env.user.company_id.id,'currency_id':
-                        price_list_object.currency_id.id, 'rate': (1 / vals['conversion_rate'])})
+                    self.env['res.currency.rate'].sudo().create(
+                        {'name': datetime.datetime.now().date(), 'company_id': self.env.user.company_id.id,
+                         'currency_id':
+                             price_list_object.currency_id.id, 'rate': (1 / vals['conversion_rate'])})
                 else:
                     self.env['res.currency.rate'].browse(currency_record[0].id).sudo().write(
                         {'rate': 1 / vals['conversion_rate']});
@@ -226,7 +396,7 @@ class InheritSaleOrder(models.Model):
     @api.model
     def write(self, vals):
         terms = \
-            "INCOTERMS : CFR\n"+\
+            "INCOTERMS : CFR\n" + \
             "PACKING:EXPORT PACKING ONTO PALLETS \n" + \
             "PAYMENT TERMS:TRANSFER IN ADVANCE IN ADVISING BANK\n" + \
             'DELIVERY PERIOD:WITHIN 2 OR 3 WEEKS \n' + \
@@ -251,8 +421,10 @@ class InheritSaleOrder(models.Model):
         if 'conversion_rate' in vals:
             if vals['conversion_rate'] and 'PKR' not in self.pricelist_id.currency_id:
                 if not currency_record:
-                    self.env['res.currency.rate'].sudo().create({'name': datetime.datetime.now().date(),'company_id':self.env.user.company_id.id ,'currency_id':
-                        self.pricelist_id.currency_id.id, 'rate': (1 / vals['conversion_rate'])})
+                    self.env['res.currency.rate'].sudo().create(
+                        {'name': datetime.datetime.now().date(), 'company_id': self.env.user.company_id.id,
+                         'currency_id':
+                             self.pricelist_id.currency_id.id, 'rate': (1 / vals['conversion_rate'])})
                 else:
                     self.env['res.currency.rate'].browse(currency_record[0].id).sudo().write(
                         {'rate': 1 / vals['conversion_rate']});
@@ -261,9 +433,10 @@ class InheritSaleOrder(models.Model):
                 pricelist = self.env['product.pricelist'].search([('id', '=', vals['pricelist_id'])])
                 if pricelist:
                     if 'PKR' not in pricelist.currency_id.name and 'BLD - Formite' in self.env.user.company_id.name:
-                        self.write({'note':terms})
+                        self.write({'note': terms})
                     else:
-                        res_config = self.env['res.config.settings'].search([('company_id', '=', self.env.user.company_id.id)],order='id DESC',limit=1)
+                        res_config = self.env['res.config.settings'].search(
+                            [('company_id', '=', self.env.user.company_id.id)], order='id DESC', limit=1)
                         if res_config:
                             self.write({'note': res_config.sale_note})
 
